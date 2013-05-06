2d5f72a18707bb054eb394336e64574ba1eb46c1
Boris.Tuman <Boris.tuman@gmail.com>
2013-05-06 14:08:07 -0400

    last project

---
+++ fpa/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fpa::Application.config.secret_token = 'fd4844d0b88215c66b13350f6d0b860b4ce8f866cee242d5cd2938eee47cf33be81700c76c2974826e1456e3b2f7641b2c24defb2d8eaf1df0d0567d14baf600'
